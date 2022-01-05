f2b2d881dadc56f4d0469f1c8256b0c1d970a1de
GabsonRoger <gabsonwona@gmail.com>
2022-01-04 22:56:59 -0300

    add test

---
+++ src/test/java/tudelft/gettingstarted/GettingStarted.java
@@ -0,0 +1,30 @@
+package tudelft.gettingstarted;
+
+import org.junit.jupiter.api.Assertions;
+import org.testng.annotations.Test;
+
+public class GettingStarted {
+    public int addFive(int number) {
+        return number + 5;
+    }
+
+
+    @Test
+    public void addFiveToMinusTwenty() {
+        int result = new GettingStarted().addFive(-20);
+        Assertions.assertEquals(-15, result);
+    }
+
+    @Test
+    public void addFiveToZero() {
+        int result = new GettingStarted().addFive(0);
+        Assertions.assertEquals(5, result);
+    }
+
+    @Test
+    public void addFiveToTwenty() {
+        int result = new GettingStarted().addFive(20);
+        Assertions.assertEquals(25,result);
+    }
+}
+
